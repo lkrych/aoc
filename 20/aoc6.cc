9230f40dc7e94cb14810307f97ddd62433b36c21
Leland Krych <leland.krych@gmail.com>
2020-12-13 12:54:18 -0800

    part 2

--- 20/aoc6.cc
+++ 20/aoc6.cc
@@ -5,24 +5,48 @@
 #include <vector>
 #include "aoc_helper.h"
 
+// part 2
+int calculateYeses(std::map<char, int> answers, int group_count) {
+    int yeses = 0;
+    std::map<char, int>::iterator it;
+
+        for ( it = answers.begin(); it != answers.end(); it++ )
+        {
+            if (it->second == group_count) {
+                yeses++;
+            }
+        }
+
+    return yeses;
+}
+
 //part1
 int yesOnCustoms (std::vector<std::string> arr) {
     int count = 0;
-    std::map<char, bool> group_answers;
+    std::map<char, int> group_answers;
+    int group_n = 0;
     for (int i = 0; i < arr.size(); i++ ){
         std::string row = arr[i];
         if (row.size() == 0) {
             // we are done parsing a group
-            count += group_answers.size();
+            count += calculateYeses(group_answers, group_n);
             group_answers.clear();
+            group_n = 0;
             continue;
         }
+        group_n++;
         for (int j = 0; j < row.size(); j++) {
             char c = row[j];
-            group_answers[c] = true;
+            if ( group_answers.find(c) == group_answers.end() ) {
+            // not found
+                group_answers[c] = 1;
+            } else {
+            // found
+                group_answers[c] += 1;
+            }
         }
     }
-    count += group_answers.size();
+    count += calculateYeses(group_answers, group_n);
     return count;
  }
 
