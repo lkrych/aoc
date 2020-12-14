e2b74d3946aed6648d89398102a572d79dcfd8d3
Leland Krych <leland.krych@gmail.com>
2020-12-14 08:23:34 -0800

    at this point, backwards compatible with part 1

--- 20/aoc7.cc
+++ 20/aoc7.cc
@@ -6,8 +6,42 @@
 #include <vector>
 #include "aoc_helper.h"
 
-std::map<std::string, std::vector<std::string>> parseBags(std::string row) {
-    std::map<std::string, std::vector<std::string>> entry;
+// part 1
+// std::map<std::string, std::vector<std::string>> parseBags(std::string row) {
+//     std::map<std::string, std::vector<std::string>> entry;
+//     // vibrant aqua bags contain 1 shiny magenta bag, 2 muted teal bags, 1 dim magenta bag, 1 muted chartreuse bag.
+//     std::vector<std::string> splitByContain = splitString(row, "bags contain");
+//     if (splitByContain.size() != 2) {
+//         std::cout << "There was a problem with your input data: " << row << std::endl;
+//         exit(1);
+//     }
+//     std::string parent = strip(splitByContain[0]);
+//     // std::cout << "container: " << key << " size: " << key.size() << std::endl;
+//     entry[parent] = {};
+//     //parse the containing bags
+//     std::vector<std::string> splitByCommas = splitString(splitByContain[1], ",");
+//     for (int i = 0; i < splitByCommas.size(); i++) {
+//         std::string unparsed = splitByCommas[i];
+//         std::string noBags = removeFromString(unparsed, {"bags", "bag", "."});
+//         std::string stripped = strip(noBags);
+//         // std::cout << stripped << " size: " << stripped.size() << std::endl;
+//         if (noBags.find("no other") != std::string::npos) {
+//             continue;
+//         }
+//         if ( entry.find(stripped) == entry.end() ) {
+//             // not found
+//                 entry[stripped] = {parent};
+//         } else {
+//         // found
+//             entry[stripped].push_back(parent);
+//         }
+//     }
+//     return entry;
+// }
+
+// part 2
+std::map<std::string, std::vector<std::pair<std::string, int>>> parseBags(std::string row) {
+    std::map<std::string, std::vector<std::pair<std::string, int>>> entry;
     // vibrant aqua bags contain 1 shiny magenta bag, 2 muted teal bags, 1 dim magenta bag, 1 muted chartreuse bag.
     std::vector<std::string> splitByContain = splitString(row, "bags contain");
     if (splitByContain.size() != 2) {
@@ -23,27 +57,30 @@ std::map<std::string, std::vector<std::string>> parseBags(std::string row) {
         std::string unparsed = splitByCommas[i];
         std::string noBags = removeFromString(unparsed, {"bags", "bag", "."});
         std::string stripped = strip(noBags);
-        // std::cout << stripped << " size: " << stripped.size() << std::endl;
+        // https://stackoverflow.com/a/30727561/4458404
+        int n = stripped[0] - '0'; // convert to int
+        stripped = strip(stripped.substr(1, stripped.size()- 1));
+        
         if (noBags.find("no other") != std::string::npos) {
             continue;
         }
         if ( entry.find(stripped) == entry.end() ) {
             // not found
-                entry[stripped] = {parent};
+                entry[stripped] = {std::pair<std::string,int> (parent, n)};
         } else {
         // found
-            entry[stripped].push_back(parent);
+            entry[stripped].push_back(std::pair<std::string,int> (parent, n));
         }
     }
     return entry;
 }
 
-int parentBags(std::map<std::string, std::vector<std::string>> adjacency_list, std::string target, std::map<std::string, bool> *parent_check) {
+int parentBags( std::map<std::string, std::vector<std::pair<std::string, int>>> adjacency_list, std::string target, std::map<std::string, bool> *parent_check) {
     int count = 0;
-    std::vector<std::string> parents = adjacency_list[target];
+    std::vector<std::pair<std::string, int>> parents = adjacency_list[target];
 
     for (int i = 0; i < parents.size(); i++) {
-        std::string parent = parents[i];
+        std::string parent = parents[i].first;
         if (parent_check->find(parent) == parent_check->end()) {
             count++;
             (*parent_check)[parent] = true;
@@ -56,11 +93,11 @@ int parentBags(std::map<std::string, std::vector<std::string>> adjacency_list, s
 
 int baggageClaim(std::vector<std::string> arr, std::string target) {
     std::string row;
-    std::map<std::string, std::vector<std::string>> adjacency_list;
+    std::map<std::string, std::vector<std::pair<std::string, int>>> adjacency_list;
     for(int i = 0; i < arr.size(); i++) {
         row = arr[i];
-        std::map<std::string, std::vector<std::string>> entry = parseBags(row);
-        std::map<std::string, std::vector<std::string>>::iterator it;
+        std::map<std::string, std::vector<std::pair<std::string, int>>> entry = parseBags(row);
+        std::map<std::string, std::vector<std::pair<std::string, int>>>::iterator it;
         for ( it = entry.begin(); it != entry.end(); it++ )
         {
             if ( adjacency_list.find(it->first) == adjacency_list.end() ) {
