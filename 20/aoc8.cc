0dce405d928b5ca93ec4f94cc538ad079e313c71
Leland Krych <leland.krych@gmail.com>
2020-12-16 11:53:41 -0800

    part 2

--- 20/aoc8.cc
+++ 20/aoc8.cc
@@ -8,16 +8,34 @@
 int findLoop(std::vector<std::string> data) {
     int acc = 0;
     int curr_ins_idx = 0;
+    int back_track_idx = 0;
+    int back_track_acc = 0;
     std::map<int,bool> past_instructions;
+    std::map<int,bool> backtrack_past_instructions;
+    std::map<int, bool> backtracked;
+    bool backtrack_in_progress = false;
     while (true) {
+        // program has reached the end
+        std::cout << "checking ins: " << curr_ins_idx << std::endl;
+        if (curr_ins_idx == data.size()) {
+            std::cout << "the end has been reached! praise be!" << std::endl;
+            return acc;
+        }
         // check if instruction has been seen before
         if ( past_instructions.find(curr_ins_idx) == past_instructions.end() ) {
             past_instructions[curr_ins_idx] = true;
         } else {
-            return acc;
+            std::cout << "discovered a loop, returning back to the old snapshot" << std::endl;
+            backtracked[back_track_idx] = true;
+            backtrack_in_progress = false;
+            //reset instruction values
+            curr_ins_idx = back_track_idx;
+            acc = back_track_acc;
+            past_instructions = backtrack_past_instructions;
         }
         // perform the instruction
         std::string curr_ins = data[curr_ins_idx];
+        std::cout << "ins: " << curr_ins << std::endl;
         std::vector<std::string> split_ins = splitString(curr_ins, " ");
         if (split_ins.size() != 2) {
             std::cout << "There was an error in the test input" << std::endl;
@@ -35,12 +53,35 @@ int findLoop(std::vector<std::string> data) {
             }
             curr_ins_idx++;
         } else if (action == "jmp") {
+            if ( backtracked.find(curr_ins_idx) == backtracked.end() && !backtrack_in_progress ) {
+                back_track_idx = curr_ins_idx;
+                back_track_acc = acc;
+                backtrack_past_instructions = past_instructions;
+                backtrack_in_progress = true;
+                //nop action
+                curr_ins_idx++;
+                continue;
+            } 
+
             if (op == '+') {
                 curr_ins_idx += amount;
             } else if (op == '-') {
                 curr_ins_idx -= amount;
             }
         } else if (action == "nop") {
+            if ( backtracked.find(curr_ins_idx) == backtracked.end() && !backtrack_in_progress) {
+                back_track_idx = curr_ins_idx;
+                back_track_acc = acc;
+                backtrack_past_instructions = past_instructions;
+                backtrack_in_progress = true;
+                //jmp action
+                if (op == '+') {
+                    curr_ins_idx += amount;
+                } else if (op == '-') {
+                    curr_ins_idx -= amount;
+                }
+                continue;
+            } 
             curr_ins_idx++;
         }
     }
