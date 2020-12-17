e9de6e4e9ef1b78e466cedd05f6c5d4b69ecca8e
Leland Krych <leland.krych@gmail.com>
2020-12-17 10:46:53 -0800

    off-by-one error, also the exclude logic wasn't necessary

--- 20/aoc9.cc
+++ 20/aoc9.cc
@@ -25,48 +25,34 @@ std::vector<std::pair<int, int>> twoSum(std::vector<int> arr, int target) {
     return vals;
 }
 
-std::vector<int> modifiedTwoSum(std::vector<int> arr, int idx, int window, std::vector<int> exclude) {
+std::vector<int> modifiedTwoSum(std::vector<long> arr, int idx, int window) {
     int target = arr[idx + window];
-    std::vector<int> subArr = {arr.begin() + idx, arr.begin() + idx + (window - 1)};
+    std::vector<int> subArr = {arr.begin() + idx, arr.begin() + idx + window};
+    
     std::vector<std::pair<int,int>> vals = twoSum(subArr, target);
     if (vals.size() == 0) {
         return {};
     }
-    // else check to make sure none of the values are excluded
-    for(int i = 0; i < vals.size(); i++) {
-        std::pair<int,int> pair = vals[i];
-        int first = pair.first;
-        int second = pair.second;
-        for (int j = 0; j < exclude.size(); j++) {
-            int excl = exclude[j];
-            if (first == excl || second == excl) {
-                // remove the pair from vals
-                vals.erase(vals.begin() + i);
-            }
-        }
-    }
     return { vals[0].first, vals[0].second };
 }
 
-int crackXmas(std::vector<int> data, int window_size) {
+int crackXmas(std::vector<long> data, int window_size) {
     int curr_idx = 0;
-    std::vector<int> exclude = {};
     while (true) {
-        std::vector<int> indices = modifiedTwoSum(data, curr_idx, window_size, exclude);
+
+        std::vector<int> indices = modifiedTwoSum(data, curr_idx, window_size);
         if (indices.size() == 0) {
             return data[curr_idx + window_size];
         }
-        for(int i = 0; i < indices.size(); i++) {
-            exclude.push_back(indices[i]);
-        }
+        
         curr_idx++;
     }
 }
 
 int main() {
     //read input from file
-    std::vector<int> data;
-    data = getInputInt("./input/aoc9.txt");
+    std::vector<long> data;
+    data = getInputLong("./input/aoc9.txt");
 
     int xmasFault = crackXmas(data, 25);
     std::cout << "the value is " << xmasFault << std::endl;
