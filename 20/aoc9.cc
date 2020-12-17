bbac761be989a7e9b7091dc27db7139267ff6708
Leland Krych <leland.krych@gmail.com>
2020-12-17 11:33:56 -0800

    finish day9

--- 20/aoc9.cc
+++ 20/aoc9.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -5,6 +6,7 @@
 #include <vector>
 #include "aoc_helper.h"
 
+// part1
 std::vector<std::pair<int, int>> twoSum(std::vector<int> arr, int target) {
     std::vector<std::pair<int, int>> vals = {};
     std::map<int, int> hash;
@@ -25,6 +27,7 @@ std::vector<std::pair<int, int>> twoSum(std::vector<int> arr, int target) {
     return vals;
 }
 
+// part1
 std::vector<int> modifiedTwoSum(std::vector<long> arr, int idx, int window) {
     int target = arr[idx + window];
     std::vector<int> subArr = {arr.begin() + idx, arr.begin() + idx + window};
@@ -36,6 +39,7 @@ std::vector<int> modifiedTwoSum(std::vector<long> arr, int idx, int window) {
     return { vals[0].first, vals[0].second };
 }
 
+// part1
 int crackXmas(std::vector<long> data, int window_size) {
     int curr_idx = 0;
     while (true) {
@@ -49,11 +53,39 @@ int crackXmas(std::vector<long> data, int window_size) {
     }
 }
 
+long sumRange(std::vector<long> data, int low, int high) {
+    long sum = 0;
+    for (int i = low; i <= high; i++) {
+        long el = data[i];
+        sum += el;
+    }
+    return sum;
+}
+
+int crackXmas2(std::vector<long> data, long target) {
+    int low_idx = 0;
+    int high_idx = 0;
+    while(true) {
+        long sum = sumRange(data, low_idx, high_idx);
+        if (sum == target) {
+            long min = *std::min_element(data.begin() + low_idx, data.begin() + high_idx);
+            long max = *std::max_element(data.begin() + low_idx, data.begin() + high_idx);
+            return min + max;
+        }
+        
+        if (sum < target) {
+            high_idx++;
+        } else if ( sum > target) {
+            low_idx++;
+        }
+    }
+}
+
 int main() {
     //read input from file
     std::vector<long> data;
     data = getInputLong("./input/aoc9.txt");
 
-    int xmasFault = crackXmas(data, 25);
+    long xmasFault = crackXmas2(data, 22477624);
     std::cout << "the value is " << xmasFault << std::endl;
 }
