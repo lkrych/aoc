66bc36aa0e37c8fab7ac219bd09d0c36c7d7f5c7
Leland Krych <leland.krych@gmail.com>
2021-01-05 15:36:05 -0800

    three sum in python

--- 20/python/aoc1.py
+++ 20/python/aoc1.py
@@ -8,11 +8,41 @@ def two_sum(arr, target):
             hash[el] = True
     return ()
 
+def three_sum(arr, target):
+    arr = sorted(arr)
+    bottom_idx = 0
+    top_idx = len(arr) - 1
+    while bottom_idx < top_idx :
+        for i in range(bottom_idx + 1, top_idx):
+            sum = arr[bottom_idx] + arr[i] + arr[top_idx]
+            if sum == target:
+                return (arr[bottom_idx], arr[i], arr[top_idx])
+            elif sum > target:
+                if (i - bottom_idx > 1):
+                    bottom_idx += 1
+                else:
+                    top_idx -= 1
+                break
+            elif top_idx - i <= 1:
+                bottom_idx += 1
+                break
+        
+    print("No three sum solution was found :(")
+    return ()
+
+# part 1 - two sum
+# if __name__ == "__main__":
+#     text_file = open("../input/aoc1.txt", "r")
+#     string_data = text_file.readlines()
+#     data = map(lambda x: int(x), string_data)
+#     ans = two_sum(data, 2020)
+#     if len(ans) > 0 :
+#         print("product: {}".format(ans[0] * ans[1]))
 
 if __name__ == "__main__":
     text_file = open("../input/aoc1.txt", "r")
     string_data = text_file.readlines()
     data = map(lambda x: int(x), string_data)
-    ans = two_sum(data, 2020)
-    if len(ans) > 0 :
-        print("product: {}".format(ans[0] * ans[1]))
+    ans = three_sum(data, 2020)
+    if len(ans) > 0:
+        print("product: {}".format(ans[0] * ans[1] * ans[2]))
