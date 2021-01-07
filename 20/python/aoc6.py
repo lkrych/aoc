87908d4b8f8b94b86db0a2e43707839045e8ef5c
Leland Krych <leland.krych@gmail.com>
2021-01-07 13:37:06 -0800

    part 2 aoc6

--- 20/python/aoc6.py
+++ 20/python/aoc6.py
@@ -1,3 +1,4 @@
+
 def customs(data):
     current_customs = {}
     q_count = 0
@@ -13,9 +14,32 @@ def customs(data):
         
     return q_count
 
+def customs2(data):
+    current_customs = {}
+    group_count = 0
+    q_count = 0
+    for line in data:
+        line = line.strip()
+        # time to check if the customs is valid
+        if len(line) < 1:
+            for c in current_customs:
+                print("checking if {} {} is {}".format(c, current_customs[c], group_count))
+                if current_customs[c] == group_count:
+                    q_count += 1
+            group_count = 0
+            current_customs = {}
+        else:
+            group_count += 1
+            for c in line:
+                if c in current_customs:
+                    current_customs[c] += 1
+                else:
+                    current_customs[c] = 1
+        
+    return q_count
 
 if __name__ == "__main__":
     text_file = open("../input/aoc6.txt", "r")
     string_data = text_file.readlines()
-    ans = customs(string_data)
+    ans = customs2(string_data)
     print("the number of customs questions is {}".format(ans))
