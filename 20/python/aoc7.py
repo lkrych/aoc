25c37f64c9b890b25e9843a95e7aeb391b384333
Leland Krych <leland.krych@gmail.com>
2021-01-07 15:42:28 -0800

    good recursive question

--- 20/python/aoc7.py
+++ 20/python/aoc7.py
@@ -1,7 +1,6 @@
 def count_parents(lug_map, target):
     to_search = [target]
     prospective_parents = {}
-    count = 0
     while len(to_search) > 0:
         current_target = to_search.pop()
         if current_target in lug_map:
@@ -37,8 +36,40 @@ def luggage(data):
 
 
 
+def count_children(lug_map, target):
+    # assume target in map
+    if len(lug_map[target]) == 0:
+        return 0
+    count = 0
+    for curr_target in lug_map[target]:
+        count += int(curr_target[1])
+        count += (int(curr_target[1]) * count_children(lug_map, curr_target[0]))
+    return count
+
+def luggage2(data):
+    luggage_map = {}
+    for line in data:
+        line = line.strip()
+        split_by_contain = line.split("bags contain")
+        parent_bag = split_by_contain[0].strip()
+        if "no other bags" in split_by_contain[1]:
+            luggage_map[parent_bag] = []
+            continue
+        split_by_comma = split_by_contain[1].split(",")
+        for bag in split_by_comma:
+            bag = bag.strip()
+            bag_split = bag.split(" ")
+            child_bag = bag_split[1] + " " + bag_split[2]
+            child_bag_count = bag_split[0]
+            if parent_bag in luggage_map:
+                luggage_map[parent_bag].append((child_bag, child_bag_count))
+            else:
+                luggage_map[parent_bag] = [(child_bag, child_bag_count)]
+        
+    return count_children(luggage_map, "shiny gold")
+
 if __name__ == "__main__":
     text_file = open("../input/aoc7.txt", "r")
     string_data = text_file.readlines()
-    ans = luggage(string_data)
-    print("the number of containing bags is {}".format(ans))
+    ans = luggage2(string_data)
+    print("the number of bags a shiny gold bags contains is {}".format(ans))
