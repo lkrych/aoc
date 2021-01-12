0e4fb2e6c10fb6cedc3ba4f7dd8efb4f9da30601
Leland Krych <leland.krych@gmail.com>
2021-01-12 11:54:13 -0800

    that took altogether too long because I didn't read the question carefully enough

--- 20/python/aoc9.py
+++ 20/python/aoc9.py
@@ -15,10 +15,33 @@ def xmas(data, window):
         if len(sum) == 0:
             return curr_val
 
+def sum_xmas(data, low, high):
+    sum = 0
+    # actually include the high index because range is exclusive
+    i = low
+    while i <= high:
+        sum += data[i]
+        i+=1
+    return sum
+
+def xmas2(data, target):
+    running_sum = 0
+    low_idx = 0
+    high_idx = 0
+    while True:
+        running_sum = sum_xmas(data, low_idx, high_idx)
+        if running_sum == target:
+            return min(data[low_idx:high_idx]) + max(data[low_idx:high_idx])
+        elif running_sum < target:
+            high_idx += 1
+        elif running_sum > target:
+            low_idx += 1
+    
+    
 
 if __name__ == "__main__":
     text_file = open("../input/aoc9.txt", "r")
     string_data = text_file.readlines()
     data = list(map(lambda x: int(x), string_data))
-    ans = xmas(data, 25)
+    ans = xmas2(data, 22477624)
     print("the number that breaks the xmas protocol is {}".format(ans))
