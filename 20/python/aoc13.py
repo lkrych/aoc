9424bdbd7bc5ecac6ef13d98e93009a9334fcfa4
Leland Krych <leland.krych@gmail.com>
2021-01-26 21:19:41 -0800

    part 2 of day13, so cool

--- 20/python/aoc13.py
+++ 20/python/aoc13.py
@@ -22,10 +22,31 @@ def shuttle1(data):
             min_bus = b
     
     return min_diff * min_bus
+
+def shuttle2(data):
+    possible_buses = data[1].strip().split(",")
+    #initialize lcm
+    lcm = int(possible_buses[0])
+    bus_time = int(possible_buses[0])
+    for i in range(1,len(possible_buses)):
+        el = possible_buses[i]
+        if el == "x":
+            continue
+        el = int(el)
+        #loop until we've found an number that satisfies our need
+        while True:
+            #lets find an overlap between the current element and the previous
+            bus_time += lcm
+            if (bus_time + i) % el == 0:
+                break
+        #update the lcm each time through the loop
+        lcm *= el
+    return bus_time
+            
         
 
 if __name__ == "__main__":
     text_file = open("../input/aoc13.txt", "r")
     string_data = text_file.readlines()
-    ans = shuttle1(string_data)
+    ans = shuttle2(string_data)
     print(f"the earliest shuttle you can take is {ans}")
