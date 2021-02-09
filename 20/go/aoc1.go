e06b316ebb5f146fb4e1d11de61f027231a3c927
Leland Krych <leland.krych@gmail.com>
2021-02-09 08:35:58 -0800

    remove some print statements and then move ReadFile into a helpers file

--- 20/go/aoc1.go
+++ 20/go/aoc1.go
@@ -1,60 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"sort"
 	"strconv"
-	"strings"
 )
 
-func main() {
-	data, err := readFile("../input/aoc1.txt")
-	if err != nil {
-		fmt.Printf("readFile failed: %v\n", err)
-	}
-	target := 2020
-	ans := threeSum(data, target)
-	fmt.Printf("Three sum of the data is %d\n", ans)
-}
-
-func readFile(filename string) ([]string, error) {
-	data := make([]string, 0)
-	file, err := os.Open(filename)
-	if err != nil {
-		return data, err
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	var line string
-	for {
-		line, err = reader.ReadString('\n')
-		if err != nil && err != io.EOF {
-			break
-		}
-
-		data = append(data, line)
-
-		if err != nil {
-			break
-		}
-	}
-	if err != io.EOF {
-		fmt.Printf(" > Failed with error: %v\n", err)
-		return data, err
-	}
-	return data, nil
-}
+// func main() {
+// 	data, err := ReadFile("../input/aoc1.txt")
+// 	if err != nil {
+// 		fmt.Printf("readFile failed: %v\n", err)
+// 	}
+// 	target := 2020
+// 	ans := threeSum(data, target)
+// 	fmt.Printf("Three sum of the data is %d\n", ans)
+// }
 
 func twoSum(data []string, target int) int {
 	m := make(map[int]bool)
 
 	for _, line := range data {
-		trimmed := strings.TrimSuffix(line, "\n")
-		n, err := strconv.Atoi(trimmed)
+		n, err := strconv.Atoi(line)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(2)
@@ -71,12 +38,11 @@ func twoSum(data []string, target int) int {
 func threeSum(data []string, target int) int {
 	intData := make([]int, 0)
 	for _, line := range data {
-		trimmed := strings.TrimSuffix(line, "\n")
-		if len(trimmed) < 1 {
+
+		if len(line) < 1 {
 			continue
 		}
-		n, err := strconv.Atoi(trimmed)
-		fmt.Println(n)
+		n, err := strconv.Atoi(line)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(2)
