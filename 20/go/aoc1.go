7b8939a0eec675291b74cc6a589cfdc7fbc6cad7
Leland Krych <leland.krych@gmail.com>
2021-02-08 21:10:13 -0800

    finish part 2 day 1

--- 20/go/aoc1.go
+++ 20/go/aoc1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,8 +16,8 @@ func main() {
 		fmt.Printf("readFile failed: %v\n", err)
 	}
 	target := 2020
-	ans := twoSum(data, target)
-	fmt.Printf("Two sum of the data is %d\n", ans)
+	ans := threeSum(data, target)
+	fmt.Printf("Three sum of the data is %d\n", ans)
 }
 
 func readFile(filename string) ([]string, error) {
@@ -62,6 +63,53 @@ func twoSum(data []string, target int) int {
 		diff := target - n
 		if _, ok := m[diff]; ok {
 			return n * diff
+		}
+	}
+	return 0
+}
+
+func threeSum(data []string, target int) int {
+	intData := make([]int, 0)
+	for _, line := range data {
+		trimmed := strings.TrimSuffix(line, "\n")
+		if len(trimmed) < 1 {
+			continue
+		}
+		n, err := strconv.Atoi(trimmed)
+		fmt.Println(n)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		intData = append(intData, n)
+	}
+	// first sort the input
+	sort.Ints(intData)
+	lower := 0
+	middle := 1
+	higher := len(intData) - 1
+	currentSum := 0
+	// create a sliding window that checks all the vals based on their sums
+	for higher > lower {
+		currentSum = intData[lower] + intData[middle] + intData[higher]
+		if currentSum == target {
+			return intData[lower] * intData[middle] * intData[higher]
+		}
+		if currentSum < target {
+			if higher-middle > 1 {
+				middle++
+			} else {
+				lower++
+				middle = lower + 1
+			}
+		} else {
+			//reset because we've gone too high, this time with the higher value decremented
+			if middle-lower > 1 {
+				lower++
+			} else {
+				higher--
+				middle = lower + 1
+			}
 		}
 	}
 	return 0
