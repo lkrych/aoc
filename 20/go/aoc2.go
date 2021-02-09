49948613a18abc520783505db9ca9ba79e26fe63
Leland Krych <leland.krych@gmail.com>
2021-02-09 08:36:09 -0800

    part 1 day 2

---
+++ 20/go/aoc2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ReadFile("../input/aoc2.txt")
+	if err != nil {
+		fmt.Printf("readFile failed: %v\n", err)
+	}
+	ans := validPasswords(data)
+	fmt.Printf("There are %d valid passwords\n", ans)
+}
+
+func validPasswords(data []string) int {
+	valid := 0
+	// example line -> 1-3 a: abcde
+	for _, line := range data {
+		if len(line) < 1 {
+			continue
+		}
+		split := strings.Split(line, " ")
+		password := split[2]
+		char := split[1][0]
+		countRange := split[0]
+		if isPasswordValid(password, char, countRange) {
+			valid++
+		}
+	}
+	return valid
+}
+
+func isPasswordValid(pass string, targetChar byte, countRange string) bool {
+	m := make(map[byte]int)
+	bs := []byte(pass)
+	// populate map count
+	for _, chr := range bs {
+		if val, ok := m[chr]; ok {
+			m[chr] = val + 1
+		} else {
+			m[chr] = 1
+		}
+	}
+	targetCount, _ := m[targetChar]
+	splitRange := strings.Split(countRange, "-")
+	low, err := strconv.Atoi(splitRange[0])
+	if err != nil {
+		fmt.Printf("There was an error converting the low range in %s \n", countRange)
+	}
+	high, err := strconv.Atoi(splitRange[1])
+	if err != nil {
+		fmt.Printf("There was an error converting the high range in %s \n", countRange)
+	}
+
+	if targetCount <= high && targetCount >= low {
+		return true
+	}
+	return false
+
+}
