2275ea95d5c5667b206b2532a5f543c9f27af371
Leland Krych <leland.krych@gmail.com>
2021-12-15 22:35:46 -0800

    finish part 2 day 3

--- 21/python/day3.py
+++ 21/python/day3.py
@@ -1,3 +1,4 @@
+from os import getgrouplist
 from pathlib import Path
 
 file_dir = Path(__file__).resolve().parent
@@ -6,11 +7,10 @@ def get_lines(filename):
     with open(f"{file_dir}/../input/{filename}") as f:
         return list(f)
 
-def power_consumption(input):
-    # nested dict pos -> int -> count 
+
+def get_pos_dict(input):
     pos_dict = {}
     for line in input:
-        line = line.strip()
         for idx, digit in enumerate(list(line)):
             dig_i = int(digit)
             if idx in pos_dict:
@@ -21,7 +21,11 @@ def power_consumption(input):
             else:
                 pos_dict[idx] = {0: 0, 1: 0}
                 pos_dict.get(idx)[dig_i] = 1
-    
+    return pos_dict
+
+def power_consumption(input):
+    # nested dict pos -> int -> count 
+    pos_dict = get_pos_dict(input)
     # (pos_dict)
     # gamma - most common bits
     # epsilon - least common bits
@@ -39,8 +43,39 @@ def power_consumption(input):
     print(f"gamma: {gamma}, epsilon: {epsilon}")
     return int(gamma, 2) * int(epsilon, 2)
 
+def get_input_by_val_idx(input, val, idx):
+    new_input = []
+    for line in input:
+        if int(line[idx]) == val:
+            new_input.append(line)
+    return new_input
+
+def life_support(input):
+    oxy_input = input.copy()
+    co2_input = input.copy()
     
+    oxy_idx = 0
+    while len(oxy_input) > 1:
+        pos_dict = get_pos_dict(oxy_input)
+        if pos_dict.get(oxy_idx).get(1) >= pos_dict.get(oxy_idx).get(0):
+            oxy_input = get_input_by_val_idx(oxy_input, 1, oxy_idx)
+        else:
+            oxy_input = get_input_by_val_idx(oxy_input, 0, oxy_idx)
+        oxy_idx += 1
+
+    co2_idx = 0
+    while len(co2_input) > 1:
+        pos_dict = get_pos_dict(co2_input)
+        if pos_dict.get(co2_idx).get(1) >= pos_dict.get(co2_idx).get(0):
+            co2_input = get_input_by_val_idx(co2_input, 0, co2_idx)
+        else:
+            co2_input = get_input_by_val_idx(co2_input, 1, co2_idx)
+        co2_idx += 1
+    
+    print(f"oxy: {oxy_input}, co2:{co2_input}")
+    return int(oxy_input[0], 2) * int(co2_input[0], 2)
 
 if __name__ == "__main__":
     lines = get_lines("day3.txt")
-    print(power_consumption(lines))
+    lines = [line.strip() for line in lines]
+    print(life_support(lines))
