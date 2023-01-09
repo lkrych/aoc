818d8812dbaa179a37349ed8ed119bd4b39c67ff
Leland Krych <leland.krych@gmail.com>
2023-01-09 10:50:53 -0800

    part 2 day 5

---
+++ 22/python/day5_2.py
@@ -0,0 +1,51 @@
+from collections import deque
+import re
+
+# read from file input
+file_name = input()
+f = open(f"/Users/lkrych/aoc/22/input/{file_name}.txt", "r")
+
+stacks = {}
+for line in f:
+    l = line.rstrip()
+    # create stacks
+    if "[" in l:
+        # replace an empty space with a placeholder text, we can then skip over this placeholder and still enumerate based off index
+        l = l.replace("    ", "placeholder ")
+        split = l.split(" ")
+        print(split)
+        for idx, el in enumerate(split, start=1):
+            #skip over the placeholders
+            if el == "placeholder" or not el:
+                continue
+            e = el.replace("[", "")
+            e = e.replace("]", "")
+            e = e.replace("placeholder", "")
+            if idx not in stacks:
+                d = deque()
+                # we are using appendleft because we are reading from the top of the stack
+                d.appendleft(e)
+                print(f"adding {e} to stack {idx}")
+                stacks[idx] = d
+            else:
+                print(f"adding {e} to stack {idx}")
+                stacks[idx].appendleft(e)
+    # once stacks are created start processing the moves
+    elif l.startswith("move"):
+        # example input: move 1 from 2 to 1
+        moves = re.findall("[0-9]+", l)
+        moves = [int(el) for el in moves]
+        num_to_move, from_col, to_col = moves
+
+        # create one more stack for saving popped items and putting them back in
+        to_move = deque()
+        for _ in range(num_to_move):
+            item = stacks[from_col].pop()
+            to_move.append(item)
+        
+        while len(to_move) > 0:
+            popped_item = to_move.pop()
+            stacks[to_col].append(popped_item)
+
+for idx in stacks.keys():
+    print(f"Idx: {idx}, item: {stacks[idx][-1]}") 
