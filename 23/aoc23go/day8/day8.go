651029372e31ad412796e29c44668947e79ca5a0
Leland Krych <leland.krych@gmail.com>
2024-02-06 08:22:08 -0800

    finish part1 of day 8

--- 23/aoc23go/day8/day8.go
+++ 23/aoc23go/day8/day8.go
@@ -3,9 +3,25 @@ package day8
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/lkrych/aoc23go/input"
 )
+
+type Node struct {
+	left  string
+	right string
+}
+
+func removeChars(s string, charsToRemove string) string {
+	var sb strings.Builder
+	for _, c := range s {
+		if !strings.ContainsRune(charsToRemove, c) {
+			sb.WriteRune(c)
+		}
+	}
+	return sb.String()
+}
 
 func Part1() {
 	// BOILERPLATE for getting file name from stdIn and reading line by line
@@ -21,14 +37,58 @@ func Part1() {
 
 	// BEGIN CODING FOR DAY HERE
 	// INITIALIZE GLOBAL VALUES
-	possibleGamesSum := 0
+	path := ""
+	graph := map[string]Node{}
+	moveOntoMap := false
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		if line == "" {
+			moveOntoMap = true
+			continue
+		}
+		if moveOntoMap {
+			// AAA = (BBB, CCC)
+			split := strings.Split(line, "=")
+			src := strings.TrimSpace(split[0])
+			dsts := removeChars(strings.TrimSpace(split[1]), "(,)")
+			leftRight := strings.Split(dsts, " ")
+
+			graph[src] = Node{
+				left:  leftRight[0],
+				right: leftRight[1],
+			}
+		} else {
+			path += line
+		}
+	}
+
+	fmt.Println("Path: ", path)
+	fmt.Println("Graph: ", graph)
+
+	currentStep := "AAA"
+	pathEls := strings.Split(path, "")
+	stepsTaken := 0
+	// Now walk the path until you reach the current step
+	for currentStep != "ZZZ" {
+		for _, el := range pathEls {
+			if currentStep == "ZZZ" {
+				break
+			}
+			stepsTaken += 1
+			currentStepNode := graph[currentStep]
+			if el == "R" {
+				currentStep = currentStepNode.right
+			} else if el == "L" {
+				currentStep = currentStepNode.left
+			} else {
+				panic("The path was neither right nor left!")
+			}
+		}
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
-	fmt.Println("Possible Games Sum: ", possibleGamesSum)
+
+	fmt.Println("Steps taken: ", stepsTaken)
 }
--- 23/aoc23go/main.go
+++ 23/aoc23go/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	d "github.com/lkrych/aoc23go/day7"
+	d "github.com/lkrych/aoc23go/day8"
 )
 
 func main() {
-	d.Part2()
+	d.Part1()
 }
